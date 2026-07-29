1b0050ed63200be1141f92430b2c37e881b6551c
zhangdi <2463312649@qq.com>
2026-07-29 15:30:48 +0800

    chore: 清理调试文件

--- functions/api/test-deploy.js
+++
@@ -1,9 +0,0 @@
-export async function onRequestGet() {
-  return new Response(JSON.stringify({
-    deployed: true,
-    timestamp: new Date().toISOString(),
-    version: '3'
-  }), {
-    headers: { 'Content-Type': 'application/json' },
-  });
-}
